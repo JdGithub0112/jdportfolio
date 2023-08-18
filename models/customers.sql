0cc73db690ca9fa4c5a021990c98dff39af35177
Jordan Davis <jordndavs@gmail.com>
2023-08-18 08:07:52 +0000

    Comitting all changes made today; the following changes are
    1. Creating Staging tables, and then using these staging tables in a modular fashion such that I only have to run the dim_customers model (dimension table, titled customers) which includes orders and customer data

--- models/customers.sql
+++ models/customers.sql
@@ -1,30 +1,12 @@
-{{
-    config(
-
-        materialized = 'table'
-    )
-}}
-
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from `dbt-tutorial`.jaffle_shop.customers
+    select * from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from `dbt-tutorial`.jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
 
 ),
 
@@ -42,7 +24,6 @@ customer_orders as (
     group by 1
 
 ),
-
 
 final as (
 
